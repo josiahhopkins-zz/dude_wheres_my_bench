7aebd33c9dfb4e9bea3d77315f182acab5f51c66
Connor <avaterq0@gmail.com>
2017-12-02 12:00:09 -0800

    SMS ready for use in main file, and added pip stuff

--- take_pictures.py
+++ take_pictures.py
@@ -1,18 +1,19 @@
 import picamera
 import time
 import os
+import send_sms
 # import <josiah>
-# import <evan>
 # import <phansa>
 
 def main():
     camera = picamera.PiCamera()
-    image_pth = 'image.jpg'
+    image_pth = 'image{}.jpg'
+    counter = 0
     try:
         while True:
             occupied = False
             send_to_aws = False
-            camera.capture(image_pth)
+            camera.capture(image_pth.format(counter%4))
             print('Taking picture at', time.gmtime().tm_min)
             # send_to_aws = <josiah>.picture(image_pth)
             print('Should I send this to aws?', send_to_aws)
@@ -22,7 +23,8 @@ def main():
                 print('Is it occupied?', occupied)
                 if not occupied:
                     print('Sending out text')
-                    # <evan>.send_text()
+                    send_sms.send()
+            counter += 1
             time.sleep(3)
     except KeyboardInterrupt:
         print('interrupted!')
