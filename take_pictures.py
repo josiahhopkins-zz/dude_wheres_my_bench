73cfb9479fa775b22b36214079259688b7c7a67e
Connor <avaterq0@gmail.com>
2017-12-02 10:44:14 -0800

    should be good

---
+++ take_pictures.py
@@ -0,0 +1,15 @@
+import piccamera
+# import <josiah class>
+
+def main():
+    camera = picamera.PiCamera()
+    image_pth = 'image.jpg'
+    try:
+        while True:
+            camera.capture(image_pth)
+            # <josiah>.picture(image_pth)
+            time.sleep(2)
+    except KeyboardInterrupt:
+        print('interrupted!')
+
+main()
