f283898b6ea1a36fd382874aeec3f68d1ca0e742
Connor <avaterq0@gmail.com>
2017-12-02 11:09:31 -0800

    tons of debug stuff

--- take_pictures.py
+++ take_pictures.py
@@ -1,16 +1,29 @@
 import picamera
 import time
-# import <josiah class>
+import os
+# import <josiah>
+# import <evan>
+# import <phansa>
 
 def main():
     camera = picamera.PiCamera()
     image_pth = 'image.jpg'
     try:
         while True:
+            occupied = False
+            send_to_aws = False
             camera.capture(image_pth)
-            print('taking picture at', time.gmtime().tm_min)
-            # <josiah>.picture(image_pth)
-            time.sleep(2)
+            print('Taking picture at', time.gmtime().tm_min)
+            # send_to_aws = <josiah>.picture(image_pth)
+            print('Should I send this to aws?', send_to_aws)
+            if send_to_aws:
+                print('Sending to aws')
+                # occupied = <phansa>.send_to_aws()
+                print('Is it occupied?', occupied)
+                if not occupied:
+                    print('Sending out text')
+                    # <evan>.send_text()
+            time.sleep(3)
     except KeyboardInterrupt:
         print('interrupted!')
 
